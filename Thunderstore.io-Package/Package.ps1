938b920f12a971eeec59708492b82349e1b8cbd4
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2022-11-16 10:55:41 -0500

    Simplified packaging

--- Thunderstore.io-Package/Package.ps1
+++ Thunderstore.io-Package/Package.ps1
@@ -1,3 +1,4 @@
-Copy-Item ..\StacklandsSkipIntro\bin\Release\netstandard20\StacklandsSkipIntro.dll -Destination .
+$ProjectName = "StacklandsSkipIntro"
+Copy-Item "..\$ProjectName\bin\Release\netstandard20\$ProjectName.dll" -Destination .
 Copy-Item ..\README.md
-Get-ChildItem -Exclude *.zip | Compress-Archive -Force -DestinationPath ./StacklandsSkipIntro.zip
+Get-ChildItem -Exclude *.zip | Compress-Archive -Force -DestinationPath ./$ProjectName.zip
