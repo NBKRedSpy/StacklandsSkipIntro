1d8f4f826b41c477c344935c5753f7ae62bce485
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2022-11-16 12:39:16 -0500

    Thunderstore.io Package file fix.
    Fixed inclusion of build script in Thunderstore.io package

--- Thunderstore.io-Package/Package.ps1
+++ Thunderstore.io-Package/Package.ps1
@@ -1,4 +1,4 @@
 $ProjectName = "StacklandsSkipIntro"
 Copy-Item "..\$ProjectName\bin\Release\netstandard20\$ProjectName.dll" -Destination .
 Copy-Item ..\README.md
-Get-ChildItem -Exclude *.zip | Compress-Archive -Force -DestinationPath ./$ProjectName.zip
+Get-ChildItem -Exclude *.zip,*.ps1 | Compress-Archive -Force -DestinationPath ./$ProjectName.zip
